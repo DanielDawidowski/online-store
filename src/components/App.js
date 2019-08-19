2e44167b69e95c7cbbdb09c7d5838e89a3199456
Daniel <dvds1987@gmail.com>
2019-08-19 15:14:00 +0200

    Optional SearchField with .filter()

--- src/components/App.js
+++ src/components/App.js
@@ -36,13 +36,19 @@ class App extends Component {
     }  
   }
 
-  handleChange = (value) => {
+  handleChange = ({value}) => {
    this.setState({searchTerm: value})
   }
 
+  filteredBrands= ({ searchTerm, brands }) => {
+    return brands.filter(brand => {
+      return brand.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      brand.description.toLowerCase().includes(searchTerm.toLowerCase()) 
+    })
+  }
 
   render() {
-    const { brands, searchTerm } = this.state;
+    const { searchTerm } = this.state;
 
 
     return (
@@ -57,6 +63,7 @@ class App extends Component {
             id="searchField"
             accessibilityLabel="Brands Search Field"
             onChange={this.handleChange}
+            value={searchTerm}
             placeholder="Search Brands"
           />
           <Box margin={2}>
@@ -92,7 +99,7 @@ class App extends Component {
         display="flex" 
         justifyContent="around"
         >
-          {brands.map(brand => (
+          {this.filteredBrands(this.state).map(brand => (
             <Box
               paddingY={4}
               margin={2}
