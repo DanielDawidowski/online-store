2a3576b08b9e43601c951029235abbe6fd8dbc7f
Daniel <dvds1987@gmail.com>
2019-08-21 11:38:06 +0200

    Add form Validation to Sign Up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,12 +10,24 @@ class Signup extends React.Component {
     }
     
     
+handleChange = ({ event, value}) => {
+    event.persist();
+    this.setState({[event.target.name]: value})
+}
+
+handleSubmit = event => {
+    event.preventDefault()
     
-    
-    handleChange = ({ event, value}) => {
-        event.persist();
-        this.setState(([event.target.name]))
+    if (!this.isFormEmpty(this.state)){
+        console.log("submitted")
     }
+}
+
+isFormEmpty = ({username, email, password}) => {
+    return !username || !email || !password;
+}
+
+
     render() {
         return (
             <Container>
@@ -36,7 +48,9 @@ class Signup extends React.Component {
                         display:'inlineBlock',
                         textAlign:'center',
                         maxWidth: 450
-                    }}>
+                    }}
+                        onSubmit={this.handleSubmit}
+                    >
                         {/* Sign Up Form Heading */}
                         <Box
                             marginBottom={2}
